d6bf53ab0ca0f2af929ffd14176894435d324c1e
soladenko <olsv@ukr.net>
2016-09-03 19:32:27 +0300

    Commit of all modules

---
+++ src/HomeWorkModule1/FourthClass.java
@@ -0,0 +1,9 @@
+package HomeWorkModule1;
+
+
+public class FourthClass {
+        public static void main(String[] args) {
+            System.out.println("Мой первый класс на Java");
+        }
+    }
+
---
+++ src/HomeWorkModule1/ThirdClass.java
@@ -0,0 +1,7 @@
+package HomeWorkModule1;
+
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
