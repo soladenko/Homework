d7c2910cb95ac7dd3297158284af4abdcbda0e99
soladenko <olsv@ukr.net>
2016-09-11 21:39:49 +0300

    Commit all files4

--- src/Module4Homework/User.java
+++ src/Module4Homework/User.java
@@ -2,5 +2,97 @@ package Module4Homework;
 
 
 public class User {
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
 
+    public User(long id, String name, double balance, int monthsOfEmployment, String companyName, int salary, Bank bank) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.bank = bank;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public User setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User setBalance(double balance) {
+        this.balance = balance;
+        return this;
+    }
+
+    public User setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+        return this;
+    }
+
+    public User setCompanyName(String companyName) {
+        this.companyName = companyName;
+        return this;
+    }
+
+    public User setSalary(int salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    public User setBank(Bank bank) {
+        this.bank = bank;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", monthsOfEmployment=" + monthsOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
 }
